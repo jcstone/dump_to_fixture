ed155ac33fda4e91f3d67ab99ea070871eb71e20
Justin Stone <justin.stone@servertech.com>
2021-09-02 10:37:17 -0700

    setup railtie

--- lib/dump_to_fixture/railtie.rb
+++ lib/dump_to_fixture/railtie.rb
@@ -1,4 +1,10 @@
 module DumpToFixture
   class Railtie < ::Rails::Railtie
+    railtie_name :dump_to_fixture
+
+    rake_tasks do
+      path = File.expand_path(__dir__)
+      Dir.glob("#{path}/tasks/**/*.rake").each { |f| load f }
+    end
   end
 end
